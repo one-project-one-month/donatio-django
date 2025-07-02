cd19df52d57680a71b91598755b67ca002b1e4ef
Aye Thin Myat <156531412+EriThinMyat@users.noreply.github.com>
2025-07-02 20:37:35 +0630

    Backend/OrganizationRequest & Organization

--- organizations/urls.py
+++ organizations/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import OrganizationRequestViewSet
+from .views import OrganizationRequestViewSet, OrganizationViewSet
 
 router = DefaultRouter()
 router.register(r'organization-requests', OrganizationRequestViewSet)
+router.register(r'organizations', OrganizationViewSet)
 
 urlpatterns = [
-    path('api/', include(router.urls)),
+    path('', include(router.urls)),
 ]
